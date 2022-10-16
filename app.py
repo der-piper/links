da44721280c4781927248ffa2f6f8f6c57ec3d2a
piper <8490493+der-piper@users.noreply.github.com>
2022-10-16 11:50:41 +0200

    bugfix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import base64
 
 background_img = Image.open("media/5350_3d-removebg.png")
 #fav_icon = Image.open("media/")
@@ -21,14 +22,21 @@ st.set_page_config(
 with open('styles.css') as style:
     st.markdown(f'<style>{style.read()}</style>', unsafe_allow_html=True)
 
-
-st.markdown(f'''<style>
-body {
-    background-image: url("media/5350_3d-removebg.png");
-    background-position: right bottom;
-    background-repeat: no-repeat;
- }
-</style>''', unsafe_allow_html=True)
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('media/5350_3d-removebg.png') 
 
 
 st.header('Piper')
